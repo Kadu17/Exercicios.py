c2dbd50748279c96e8b9b316b981937a17e40e04
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-03-09 08:18:26 -0300

    Update Sorteador.py

--- Sorteador.py
+++ Sorteador.py
@@ -3,31 +3,31 @@ from threading import Timer
 
 
 
-def tempo(texto="Que pena, seu tempo esgotou!"):
+def tempo(texto="Tente novamente, seu tempo esgotou!"):
     print(f'Fim de jogo -- {texto}')
-    pid = os.getpid()
-    os.kill(pid, -1)
+
+
 
 
 temporizador = Timer(60, tempo)
 temporizador.start()
-s = random.randint(0, 100)
+s = random.randint(0, 1000)
 
-vida = 50
-t = 3
+vida = 1000
+tentativas = 15
 
 print('Tente adivinhar o número!')
 print('-'*36)
 
-while t != 0 or vida != 0:
+while tentativas != 0 or vida != 0:
     print(s)
     n = int(input('Digite um número de sua escolha: '))
-    t -= 1
+    tentativas -= 1
     chute = abs(s - n)
     vida -= chute
     print('Vida: ', vida)
     if vida <= 0:
-        print('Que pena! Suas vidas esgotaram!')
+        print('Suas vidas esgotaram!')
         break
     if n != s:
         print('Você errou!')
@@ -36,10 +36,8 @@ while t != 0 or vida != 0:
         print('Você acertou!')
         break
     if t == 0:
-        print('Você esgotou todas as suas chances!')
+        print('Você acabou com todas as suas chances!')
         break
 print('-'*36)
-
-
 
 
