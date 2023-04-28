69b5b1665f89784c23dadb177cebe38e4cd40e69
Kadu17 <118760023+Kadu17@users.noreply.github.com>
2023-04-28 16:50:59 -0300

    Quem_sou.py

--- Quem_sou.py
+++ Quem_sou.py
@@ -9,11 +9,11 @@ class Jogo:
         self.__pontos = 25
 
     def lista(self):
-        lista_de_dicas = [['BUZZ LIGHTYEAR', 'Um brinquedo do Wandy', 'Patrulheiro espacial', '"Ao infinito e alem"'],
-                          ['ARIEL', 'Vive no mar', 'Ruiva', 'Sereia'],
-                          ['PETER PAN', 'Menino de espirito livre e travesso', 'Nunca cresce', 'Terra do nunca...'],
-                          ['OLAF', 'Gosta de abraços quentinhos', 'Foi criado por uma princesa', 'Boneco de neve'],
-                          ['CINDERELA', 'Loira', 'Madrasta má', 'Sapatinho de cristal']]
+        lista_de_dicas = [['BUZZ LIGHTYEAR', 'Um brinquedo do Wandy', 'Tem um laser no pulso', 'Melhor amigo do Woody', 'Patrulheiro espacial', '"Ao infinito e além"'],
+                          ['ARIEL', 'Vive no mar', 'Filha do rei Tritão', 'Melhor amiga de um peixe', 'Ruiva', 'Sereia'],
+                          ['PETER PAN', 'Menino de espirito livre e travesso','Aventureiro e ousado', 'É melhor amigo de uma fada', 'Nunca cresce', 'Terra do nunca...'],
+                          ['OLAF', 'Gosta de abraços quentinhos', 'Nariz de cenoura', 'Foi criado por uma princesa','Usa galhos como braço', 'Boneco de neve'],
+                          ['CINDERELA','Menina órfã', 'Loira', 'Madrasta má', 'Sapatinho de cristal', 'Fada madrinha']]
 
         return lista_de_dicas
 
@@ -24,25 +24,40 @@ class Jogo:
 
         palpite = input("Digite o seu palpite: ").upper()
         if palpite == palavra_certa:
-            print("Parabéns, você acertou!")
-            print(f"Seus pontos foram {self.__pontos}")
+            print("Parabéns,você acertou!")
+            print(f"{n} fez {self.__pontos} pontos ")
             exit()
 
         else:
             if self.__cont == 1:
-                print(dicas[1])
+                print(dicas[0])
                 self.__cont += 1
                 self.__pontos -= 5
 
             elif self.__cont == 2:
-                print(dicas[2])
+                print(dicas[1])
                 self.__cont += 1
                 self.__pontos -= 5
 
             elif self.__cont == 3:
+                print(dicas[2])
+                self.__cont += 1
+                self.__pontos -= 5
+
+            elif self.__cont == 4:
                 print(dicas[3])
                 self.__cont += 1
                 self.__pontos -= 5
+
+            elif self.__cont == 5:
+                print(dicas[4])
+                if dicas != palavra_certa:
+                    print("Você perdeu!!")
+                    self.__pontos -= 5
+                    print(f"{n} fez {self.__pontos} pontos")
+                    exit()
+
+
             else:
                 print('Você perdeu')
                 exit()
@@ -58,17 +73,22 @@ lista = jogo.lista()
 
 abertura2 = input("|---------------------------------------------|\n"
                   "|               SEJA BEM VINDO                |\n"
+                  "|                                             |\n"
                   "|    O TEMA DO JOGO É PERSONAGENS DA DISNEY   |\n"
+                  "|                                             |\n"
                   "|       PRESSIONE ENTER PARA CONTINUAR        |\n"
+                  "|                                             |\n"
                   "|---------------------------------------------|\n")
 
 n = input("\t\t Digite o seu nome: ").upper()
 
 
-abertura1 =print("|---------------------------------------------|\n"
-                f"|               OLÁ {n}                      |\n"
-                 "|          VAMOS COMEÇAR O JOGO!              |\n" 
-                 "|---------------------------------------------|\n")
+abertura1 =print("|--------------------------------------------|\n"
+                f"|               OLÁ {n}                     |\n"
+                 "|                                            |\n"
+                 "|          VAMOS COMEÇAR O JOGO!             |\n" 
+                 "|--------------------------------------------|\n")
 personagem_escolhido = random.choice(lista)
 while True:
-    jogo.palpite(personagem_escolhido[0], personagem_escolhido[1:4])
+
+    jogo.palpite(personagem_escolhido[0], personagem_escolhido[1:6])
